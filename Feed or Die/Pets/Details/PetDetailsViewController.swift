4603b75508493bb42bbe7a5a0ac71db9eaf78022
Boomesh Bohn <sumesh@bnotions.com>
2016-04-28 10:03:14 -0400

    Added details, and started to format pet screen

---
+++ Feed or Die/Pets/Details/PetDetailsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PetDetailsViewController.swift
+//  Feed or Die
+//
+//  Created by Sumesh John on 4/28/16.
+//  Copyright © 2016 Boomesh Bohn. All rights reserved.
+//
+
+import UIKit
+
+class PetDetailsViewController: UIViewController {
+
+}
